8547fe3333fa8671a351343edb1ac25722c30df9
Sara Gorecki <sgorecki@gmail.com>
2013-11-14 23:21:41 -0500

    add message stating if user is getting warmer or colder

--- Number-Guesser/number-guesser.rb
+++ Number-Guesser/number-guesser.rb
@@ -1,8 +1,10 @@
 def number_guesser
 	puts "Guess a number between 0 and 100."
 	random = rand(1..100)
+	last_diff = 0
 	loop do 
 		guess = gets.chomp.to_i
+		diff = random - guess
 		if guess == random
 			puts "Congratulations! That was the right number! Do you want to play again? Yes or no?"
 			play_again = gets.chomp.downcase
@@ -19,7 +21,9 @@ def number_guesser
 		else
 			puts "Sorry, that number is too low. I was guessing #{random}."
 		end
+		puts diff.abs < last_diff ? "You're getting warmer!" : "You're getting colder."
 		puts "Guess again!"
+		last_diff = diff.abs
 	end
 end
 
