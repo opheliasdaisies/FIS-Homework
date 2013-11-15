e2bc324d35421af29fe68a136ae7afef94920634
Sara Gorecki <sgorecki@gmail.com>
2013-11-14 22:55:03 -0500

    added loop to continue playing if wrong guess

--- Number-Guesser/number-guesser.rb
+++ Number-Guesser/number-guesser.rb
@@ -1,13 +1,16 @@
 def number_guesser
 	puts "Guess a number between 0 and 100."
-	guess = gets.chomp.to_i
-	random = rand(1..100)
-	if guess == random
-		puts "Congratulations! That was the right number!"
-	elsif guess > random
-		puts "Sorry, that number is too high. I was guessing #{random}."
-	else
-		puts "Sorry, that number is too low. I was guessing #{random}."
+	loop do 
+		guess = gets.chomp.to_i
+		random = rand(1..100)
+		if guess == random
+			puts "Congratulations! That was the right number!"
+			break
+		elsif guess > random
+			puts "Sorry, that number is too high. I was guessing #{random}. Guess again!"
+		else
+			puts "Sorry, that number is too low. I was guessing #{random}. Guess again!"
+		end
 	end
 end
 
