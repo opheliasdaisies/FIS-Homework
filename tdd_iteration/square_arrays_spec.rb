f3bbe07f2c4824c97ceab4aabfb25f2b65a0fb01
Sara Gorecki <sgorecki@gmail.com>
2013-11-19 18:55:06 -0500

    correct math error in test expectation

--- tdd_iteration/square_arrays_spec.rb
+++ tdd_iteration/square_arrays_spec.rb
@@ -4,7 +4,7 @@ describe "#square_array" do
 	it "should return [25, 4, 49, 36] if passed [5, 2, 7, 6]" do
 		expect(square_array([5, 2, 7, 6])).to eq([25, 4, 49, 36])
 	end
-	it "should return [1, 4, 9, 16, 25, 36, 42] if passed [1, 2, 3, 4, 5, 6, 7]" do
-		expect(square_array([1, 2, 3, 4, 5, 6, 7])).to eq([1, 4, 9, 16, 25, 36, 42])
+	it "should return [1, 4, 9, 16, 25, 36, 49] if passed [1, 2, 3, 4, 5, 6, 7]" do
+		expect(square_array([1, 2, 3, 4, 5, 6, 7])).to eq([1, 4, 9, 16, 25, 36, 49])
 	end
 end
