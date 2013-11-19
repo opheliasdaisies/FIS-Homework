28a9fd765e139aef6d27aafc61aa2611ccbc4aac
Sara Gorecki <sgorecki@gmail.com>
2013-11-18 19:41:14 -0500

    add a supply to a winter holiday

--- holiday_suppliers/holiday_suppliers.rb
+++ holiday_suppliers/holiday_suppliers.rb
@@ -14,4 +14,8 @@ holiday_supplies = {
   }
 }
 
+#How would you access the second supply for the forth_of_july?
 holiday_supplies[:summer][:forth_of_july][1]
+
+#Add a supply to a Winter holiday.
+holiday_supplies[:winter][:new_years] << "Confetti"
