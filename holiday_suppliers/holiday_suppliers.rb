14fe762c399298436705b8b381a6da94216a7a96
Sara Gorecki <sgorecki@gmail.com>
2013-11-18 19:36:45 -0500

    add holiday_suppliers.rb file with initial hashes

---
+++ holiday_suppliers/holiday_suppliers.rb
@@ -0,0 +1,15 @@
+holiday_supplies = {
+  :winter => {
+    :christmas => ["Lights", "Wreath"],
+    :new_years => ["Party Hats"]
+  },
+  :summer => {
+    :forth_of_july => ["Fireworks", "BBQ"]
+  },
+  :fall => {
+    :thanksgiving => ["Turkey"]
+  },
+  :spring => {
+    :memorial_day => ["BBQ"]
+  }
+}
