f45591534eded1e3ed0da6a6998851414a93c314
Sara Gorecki <sgorecki@gmail.com>
2013-11-19 22:04:28 -0500

    correct Array tests to be able to be called on the array class.

--- tdd_iteration/max_spec.rb
+++ tdd_iteration/max_spec.rb
@@ -15,17 +15,17 @@ describe "#maximum" do
 	end
 end
 
-# describe Array, "#maximum" do
-# 	it "should return the largest element of the array" do
-# 		expect(maximum([43, 32, 45, 23])).to eq(45)
-# 	end
-# 	it "should return the largest element of the array" do
-# 		expect(maximum([4, 0, 1, 2, 3])).to eq(4)
-# 	end
-# 	it "should return an empty array if given an empty array" do
-# 		expect(maximum([])).to eq([])
-# 	end
-# 	it "should return the largest element of the array if two elements are the same" do
-# 		expect(maximum([1, 2, 2, 1])).to eq(2)
-# 	end
-# end
+describe Array, "#maximum" do
+	it "should return the largest element of the array" do
+		expect([43, 32, 45, 23].maximum).to eq(45)
+	end
+	it "should return the largest element of the array" do
+		expect([4, 0, 1, 2, 3].maximum).to eq(4)
+	end
+	it "should return an empty array if given an empty array" do
+		expect([].maximum).to eq([])
+	end
+	it "should return the largest element of the array if two elements are the same" do
+		expect([1, 2, 2, 1].maximum).to eq(2)
+	end
+end
