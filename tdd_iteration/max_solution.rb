bd9ddbe18bc383d5bad03bf8d96c06d60f9ae65b
Sara Gorecki <sgorecki@gmail.com>
2013-11-19 21:51:29 -0500

    add code to return empty array if empty array is passed in

--- tdd_iteration/max_solution.rb
+++ tdd_iteration/max_solution.rb
@@ -3,14 +3,19 @@
 # part II. reimplement this method as an instance method on the array class
 
 def maximum(arr)
-	arr.sort!
-	arr.last
+	if arr.length == 0
+		[]
+	else
+		arr.sort!
+		arr.last
+	end
 end
 
 
 # now rewrite this as a method on the array class
-class Array
-  def maximum
-
-  end
-end
+# class Array
+#   def maximum
+# 	#arr.sort!
+# 	#arr.last
+#   end
+# end
